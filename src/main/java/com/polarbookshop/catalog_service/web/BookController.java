4a6a8b7be2c04f93c8bed78dfef4efc4e64f826a
M.St <mst@fictious.com>
2025-02-18 19:46:00 +0100

    Correct the path for the post method

--- src/main/java/com/polarbookshop/catalog_service/web/BookController.java
+++ src/main/java/com/polarbookshop/catalog_service/web/BookController.java
@@ -1,10 +1,44 @@
 package com.polarbookshop.catalog_service.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.polarbookshop.catalog_service.domain.entity.Book;
+import com.polarbookshop.catalog_service.domain.service.BookService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("books")
 public class BookController {
-    
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public Iterable<Book> get() {
+        return bookService.viewBookList();
+    }
+
+    @GetMapping("{isbn}")
+    public Book get(@PathVariable String isbn) {
+        return bookService.viewBookDetails(isbn);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book post(@RequestBody Book book) {
+        return bookService.addBookToCatalog(book);
+    }
+
+    @DeleteMapping("{isbn}")
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void delete(@PathVariable String isbn) {
+        bookService.removeBookFromCatalog(isbn);
+    }
+
+    @PutMapping("{isbn}")
+    public Book put(@PathVariable String isbn, @RequestBody Book book) {
+        return bookService.editBookDetails(isbn, book);
+    }
 }
