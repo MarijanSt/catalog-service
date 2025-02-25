c182cd4c5081ee4d81e6195ab2ea8ae8e8f1ac8e
M.St <mst@fictious.com>
2025-02-25 14:36:01 +0100

    Enclose with try-resources

--- src/test/java/com/polarbookshop/catalog_service/BookValidationTest.java
+++ src/test/java/com/polarbookshop/catalog_service/BookValidationTest.java
@@ -19,8 +19,9 @@ public class BookValidationTest {
 
     @BeforeAll
     static void setUp() {
-        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-        validator = factory.getValidator();
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            validator = factory.getValidator();
+        }
     }
 
     @Test
