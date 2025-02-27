577bec221285bb343e5b3d65af0640b941d39c0e
M.St <mst@fictious.com>
2025-02-27 19:47:41 +0100

    Remove unused imports

--- src/main/java/com/polarbookshop/catalog_service/web/BookControllerAdvice.java
+++ src/main/java/com/polarbookshop/catalog_service/web/BookControllerAdvice.java
@@ -4,7 +4,6 @@ import com.polarbookshop.catalog_service.domain.exception.BookAlreadyExistsExcep
 import com.polarbookshop.catalog_service.domain.exception.BookNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
